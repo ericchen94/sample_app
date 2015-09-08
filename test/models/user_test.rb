0cdbcc891e4fac3129e155d2036b9dbfa6bbb11e
Eric Chen <ericchenwon@gmail.com>
2015-09-08 18:15:36 +0000

    Validate length of name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,9 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "    "
     assert_not @user.valid?
   end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
